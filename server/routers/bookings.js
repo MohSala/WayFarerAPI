973f2e55aaca3180e56193a88cd4388ad34313ff
Moh <moh@Mohs-MacBook-Pro.local>
2019-07-13 23:08:30 +0100

    [feature #167276873] User can delete his booking

--- server/routers/bookings.js
+++ server/routers/bookings.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post("/bookings", BookingController.createBooking);
 router.get("/bookings", BookingController.viewBookings);
+router.delete("/bookings/:booking_id", BookingController.deleteBooking);
 
 export default router;
